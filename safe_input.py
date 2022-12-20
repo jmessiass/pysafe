4f34f08496ab8ff90523dff7c070ab2b1a13eba8
Raphael Fleury <raphael.fleury@fcamara.com.br>
2022-12-20 16:49:35 -0300

    refactor: using recursive functions

--- safe_input.py
+++ safe_input.py
@@ -4,34 +4,61 @@ import html
 import sys
 import re
 
-def sanitize_input(param):
-    secure_data = {}
-    status_data = False
-    data = dict(literal_eval(param)) # DEV
-    # data = dict(param) # PROD
 
-    for key in data:
-        input = data[key] # ATRIBUI O VALOR DIGITADO PARA A VAR INPUT
-        safe_code1 = html.escape(input) if type(input) != int else input # ENCODA CARACTERES DE TAG HTML
-        safe_code2 = str(safe_code1) # FORCA O VALOR SER UMA STRING
+def sanitize(value):
+    if isinstance(value, list):
+        return sanitize_list(value)
+    if isinstance(value, dict):
+        return sanitize_dict(value)
+    if isinstance(value, str):
+        return sanitize_str(value)
 
-        if len(safe_code2) > 100: # SE A QUANTIDADE DE CARACTERES INSERIDOS FOR MAIOR QUE 100, RETORNA FALSO E MENSAGEM DE ERRO
-            secure_data.update({key: "a quantidade de caracteres inseridos ultrapassa o limite de 100"})
-            status_data = False
-            return status_data, secure_data
+    return True, value
 
-        # VALIDA SE FOI DIGITADO APENAS LETRAS NUMEROS E . : /
-        if not bool(re.match("^[A-Za-z0-9./:]*$", safe_code2)):
-                # import ipdb;ipdb.set_trace()
-                secure_data.update({key: "foi inserido um caractere invalido"})
-                status_data = False
-                return status_data, secure_data
 
-        secure_data.update({key: safe_code2})  # ATUALIZA DICIONARIO COM OS DADOS TRATADOS
+def sanitize_dict(data: dict):
+    try:
         status_data = True
+        secure_data = {}
 
-    return status_data, secure_data
+        for key in data:
+            value = data[key]  # ATRIBUI O VALOR DIGITADO PARA A VAR INPUT
+            status_value, secure_value = sanitize(value)
+            secure_data[key] = secure_value
+            if not status_value:
+                status_data = False
+
+        return status_data, secure_data
+
+    except Exception as error:
+        secure_data.update({"error": error})
+        return False, secure_data
+
+
+def sanitize_list(data: list):
+    status = True
+    new_list = []
+
+    for value in data:
+        status_data, secure_input = sanitize(value)
+        new_list.append(secure_input)
+        if not status_data:
+            status = False
+
+    return status, new_list
+
+
+def sanitize_str(value: str):
+    value = html.escape(value)  # ENCODA CARACTERES DE TAG HTML
+    if len(value) > 100:
+        return False, "a quantidade de caracteres inseridos ultrapassa o limite de 100"
+    if not bool(re.match("^[A-Za-z0-9./:\s]*$", value)):
+        return False, "foi inserido um caractere invalido"
+
+    return True, value
+
 
 if __name__ == "__main__":
-    response = sanitize_input(sys.argv[1])
+    data = literal_eval(sys.argv[1])
+    response = sanitize(data)
     print(response)
